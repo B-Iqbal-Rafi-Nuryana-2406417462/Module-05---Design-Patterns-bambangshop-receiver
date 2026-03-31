d9cc464f2c5655bb35b3aab87af3cf577b3fa474
Iqbal <raflyiqbal1409@gmail.com>
2026-03-31 12:24:51 +0700

    Implement list function in Notification controller

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -79,7 +79,7 @@ impl NotificationService {
         return Ok(subscriber_result);
     }
 
-    pub fn list_message() -> Result<Vec<String>> {
+    pub fn list_messages() -> Result<Vec<String>> {
         return Ok(NotificationRepository::list_all_as_string());
     }
 }
